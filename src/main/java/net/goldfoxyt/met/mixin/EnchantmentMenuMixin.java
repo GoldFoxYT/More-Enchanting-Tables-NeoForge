cc39debf31ce8477fd899a46f8024918c6d12832
GoldFoxYT <goldfoxyt1@outlook.com>
2024-07-27 18:03:45 +0300

    added mixins

--- src/main/java/net/goldfoxyt/met/mixin/EnchantmentMenuMixin.java
+++ src/main/java/net/goldfoxyt/met/mixin/EnchantmentMenuMixin.java
@@ -3,6 +3,7 @@ package net.goldfoxyt.met.mixin;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.EnchantmentMenu;
+import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -14,8 +15,8 @@ public class EnchantmentMenuMixin {
     @Shadow @Final private Container enchantSlots;
 
     /**
-     * @author  GoldFoxYT
-     * @reason  To put the custom enchanting tables in the stillValid method.
+     * @author GoldFoxYT
+     * @reason To put the custom enchanting tables in the stillValid method.
      */
     @Overwrite
     public boolean stillValid(Player player) {
